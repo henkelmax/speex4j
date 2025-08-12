9a1305a2e73c17fad866427c65d7792ba56b6922
Max Henkel <max@maxhenkel.de>
2025-08-12 15:59:53 +0200

    Improve unit tests

--- src/test/java/de/maxhenkel/rnnoise4j/AutomaticGainControlTest.java
+++ src/test/java/de/maxhenkel/rnnoise4j/AutomaticGainControlTest.java
@@ -16,14 +16,14 @@ public class AutomaticGainControlTest {
     void agc() throws IOException, UnknownPlatformException {
         int frameSize = 960;
         int sampleRate = 48000;
-        try (AutomaticGainControl denoiser = new AutomaticGainControl(frameSize, sampleRate)) {
+        try (AutomaticGainControl agc = new AutomaticGainControl(frameSize, sampleRate)) {
             short[] shorts = new short[frameSize];
             short[] bufferBefore = new short[frameSize];
             short[] bufferAfter = new short[frameSize];
             for (int i = 0; i < shorts.length; i += bufferBefore.length) {
                 System.arraycopy(shorts, i, bufferBefore, 0, bufferBefore.length);
                 System.arraycopy(shorts, i, bufferAfter, 0, bufferAfter.length);
-                denoiser.agc(bufferAfter);
+                agc.agc(bufferAfter);
                 assertEquals(bufferBefore.length, bufferAfter.length);
                 assertNotEquals(bufferBefore, bufferAfter);
             }
@@ -33,25 +33,109 @@ public class AutomaticGainControlTest {
     @Test
     @DisplayName("Set valid target")
     void validTarget() throws IOException, UnknownPlatformException {
-        try (AutomaticGainControl denoiser = new AutomaticGainControl(960, 48000)) {
-            denoiser.setTarget(1);
-            denoiser.setTarget(8000);
-            denoiser.setTarget(32768);
+        try (AutomaticGainControl agc = new AutomaticGainControl(960, 48000)) {
+            agc.setTarget(1);
+            agc.setTarget(8000);
+            agc.setTarget(32768);
         }
     }
 
     @Test
     @DisplayName("Set invalid target")
     void invalidTarget() throws IOException, UnknownPlatformException {
-        try (AutomaticGainControl denoiser = new AutomaticGainControl(960, 48000)) {
-            IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> denoiser.setTarget(0));
+        try (AutomaticGainControl agc = new AutomaticGainControl(960, 48000)) {
+            IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> agc.setTarget(0));
             assertEquals("Invalid target (must be 1..32768)", e.getMessage());
-            IllegalArgumentException e1 = assertThrows(IllegalArgumentException.class, () -> denoiser.setTarget(32769));
+            IllegalArgumentException e1 = assertThrows(IllegalArgumentException.class, () -> agc.setTarget(32769));
             assertEquals("Invalid target (must be 1..32768)", e1.getMessage());
-            IllegalArgumentException e2 = assertThrows(IllegalArgumentException.class, () -> denoiser.setTarget(-1));
+            IllegalArgumentException e2 = assertThrows(IllegalArgumentException.class, () -> agc.setTarget(-1));
             assertEquals("Invalid target (must be 1..32768)", e2.getMessage());
-            IllegalArgumentException e3 = assertThrows(IllegalArgumentException.class, () -> denoiser.setTarget(Short.MIN_VALUE));
+            IllegalArgumentException e3 = assertThrows(IllegalArgumentException.class, () -> agc.setTarget(Short.MIN_VALUE));
             assertEquals("Invalid target (must be 1..32768)", e3.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Get target")
+    void getTarget() throws IOException, UnknownPlatformException {
+        try (AutomaticGainControl denoiser = new AutomaticGainControl(960, 48000)) {
+            assertEquals(8000, denoiser.getTarget());
+            denoiser.setTarget(1);
+            assertEquals(1, denoiser.getTarget());
+            denoiser.setTarget(32768);
+            assertEquals(32768, denoiser.getTarget());
+        }
+    }
+
+    @Test
+    @DisplayName("Set max gain")
+    void setMaxGain() throws IOException, UnknownPlatformException {
+        try (AutomaticGainControl agc = new AutomaticGainControl(960, 48000)) {
+            IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> agc.setMaxGain(-1));
+            assertEquals("Invalid max gain (must be >= 0)", e.getMessage());
+            agc.setMaxGain(0);
+            agc.setMaxGain(Short.MAX_VALUE);
+        }
+    }
+
+    @Test
+    @DisplayName("Get max gain")
+    void getMaxGain() throws IOException, UnknownPlatformException {
+        try (AutomaticGainControl agc = new AutomaticGainControl(960, 48000)) {
+            assertEquals(30, agc.getMaxGain());
+            agc.setMaxGain(1);
+            assertEquals(1, agc.getMaxGain());
+            agc.setMaxGain(128);
+            assertEquals(128, agc.getMaxGain());
+        }
+    }
+
+    @Test
+    @DisplayName("Set increment")
+    void setIncrement() throws IOException, UnknownPlatformException {
+        try (AutomaticGainControl agc = new AutomaticGainControl(960, 48000)) {
+            IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> agc.setIncrement(-1));
+            assertEquals("Invalid increment (must be >= 0)", e.getMessage());
+            agc.setIncrement(0);
+            agc.setIncrement(Short.MAX_VALUE);
+        }
+    }
+
+    @Test
+    @DisplayName("Get increment")
+    void getIncrement() throws IOException, UnknownPlatformException {
+        try (AutomaticGainControl agc = new AutomaticGainControl(960, 48000)) {
+            assertEquals(12, agc.getIncrement());
+            agc.setIncrement(1);
+            assertEquals(1, agc.getIncrement());
+            agc.setIncrement(128);
+            assertEquals(128, agc.getIncrement());
+        }
+    }
+
+    @Test
+    @DisplayName("Set decrement")
+    void setDecrement() throws IOException, UnknownPlatformException {
+        try (AutomaticGainControl agc = new AutomaticGainControl(960, 48000)) {
+            IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> agc.setDecrement(1));
+            assertEquals("Invalid decrement (must be <= 0)", e.getMessage());
+            agc.setDecrement(0);
+            agc.setDecrement(-128);
+            agc.setDecrement(Short.MIN_VALUE);
+        }
+    }
+
+    @Test
+    @DisplayName("Get decrement")
+    void getDecrement() throws IOException, UnknownPlatformException {
+        try (AutomaticGainControl agc = new AutomaticGainControl(960, 48000)) {
+            assertEquals(-40, agc.getDecrement());
+            agc.setDecrement(0);
+            assertEquals(0, agc.getDecrement());
+            agc.setDecrement(-1);
+            assertEquals(-1, agc.getDecrement());
+            agc.setDecrement(-128);
+            assertEquals(-128, agc.getDecrement());
         }
     }
 
